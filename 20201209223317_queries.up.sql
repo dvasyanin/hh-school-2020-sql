6cdb2926f325d450305a00f3bb72d3779c21f4e6
Dmitry Vasyanin <dvasyanin0@gmail.com>
2020-12-23 23:57:57 +0300

    refactoring after review

--- 20201209223317_queries.up.sql
+++ 20201209223317_queries.up.sql
@@ -1,53 +1,69 @@
-SELECT hv.position_name,
-       ha.city,
-       he.name,
-       hv.created_at
-FROM hh.vacancy hv
-         LEFT JOIN hh.area ha on hv.area_id = ha.id
-         LEFT JOIN hh.employer he on hv.employer_id = he.id
-WHERE hv.compensation_to IS NULL
-  AND hv.compensation_from IS NULL
+SELECT
+    hv.position_name,
+    ha.city,
+    he.name,
+    hv.created_at
+FROM hh.vacancy AS hv
+LEFT JOIN hh.area AS ha on hv.area_id = ha.id
+LEFT JOIN hh.employer AS he on hv.employer_id = he.id
+WHERE
+    hv.compensation_to IS NULL
+    AND hv.compensation_from IS NULL
 ORDER BY hv.created_at DESC
 LIMIT 10;
 ---------------------------------------------------------
-SELECT AVG(compensation_to / (CASE
-                                  WHEN compensation_gross
-                                      THEN 0.87
+SELECT
+    AVG(compensation_to / (CASE WHEN compensation_gross
+                              THEN 0.87
+                              ELSE 1
+                        END)):: BIGINT AS avg_compensation_max,
+    AVG(compensation_from / (CASE WHEN compensation_gross
+                                  THEN 0.87
                                   ELSE 1
-    END)):: BIGINT        AS avg_compensation_max,
-       AVG(compensation_from / (CASE
-                                    WHEN compensation_gross
-                                        THEN 0.87
-                                    ELSE 1
-           END)):: BIGINT AS avg_compensation_min,
-       AVG(compensation_to / (CASE
-                                  WHEN compensation_gross
-                                      THEN 0.87
-                                  ELSE 1
-           END):: BIGINT - compensation_from / (CASE
-                                                    WHEN compensation_gross
-                                                        THEN 0.87
-                                                    ELSE 1
-           END):: BIGINT) AS avg_compensation_mid
+                          END)):: BIGINT AS avg_compensation_min,
+    AVG(compensation_to / (CASE WHEN compensation_gross
+                                THEN 0.87
+                                ELSE 1
+                        END):: BIGINT - compensation_from / (CASE WHEN compensation_gross
+                                                                  THEN 0.87
+                                                                  ELSE 1
+                                                            END):: BIGINT) AS avg_compensation_mid
 FROM hh.vacancy;
 ---------------------------------------------------------
-SELECT he.name, hv.position_name, count(hr) AS count
-FROM hh.vacancy hv
-         LEFT JOIN hh.response hr ON hv.id = hr.vacancy_id
-         LEFT JOIN hh.employer he ON hv.employer_id = he.id
+SELECT
+    he.name,
+    hv.position_name,
+    count(hr) AS count
+FROM hh.employer AS he
+LEFT JOIN hh.vacancy AS hv on he.id = hv.employer_id
+LEFT JOIN hh.response AS hr ON hv.id = hr.vacancy_id
 GROUP BY he.name, hv.id
 ORDER BY count DESC, name
 LIMIT 5;
 ---------------------------------------------------------
-SELECT percentile_cont(0.5) WITHIN GROUP (ORDER BY res.count)
-FROM (SELECT count(*) AS count
-      FROM hh.employer he
-                LEFT JOIN hh.vacancy hv ON he.id = hv.employer_id
-      GROUP BY he.name) AS res;
+SELECT
+    percentile_cont(0.5) WITHIN GROUP (ORDER BY res.count)
+FROM (SELECT
+          count(*) AS count
+      FROM hh.employer AS he
+      LEFT JOIN hh.vacancy AS hv ON he.id = hv.employer_id
+      GROUP BY he.id) AS res;
 ---------------------------------------------------------
-SELECT result.city, min(result.difference) AS min_response, max(result.difference) AS max_response
-FROM (SELECT ha.city, hv.created_at, hr.created_at, (hr.created_at - hv.created_at) AS difference
-      FROM hh.response hr
-               LEFT JOIN hh.vacancy hv ON hr.vacancy_id = hv.id
-               LEFT JOIN hh.area ha ON hv.area_id = ha.id) AS result
-GROUP BY result.city;
+WITH calculate_query AS (
+    SELECT
+        hv.id,
+        hv.area_id,
+        min(hr.created_at - hv.created_at) AS difference_time
+    FROM hh.vacancy AS hv
+             INNER JOIN hh.response AS hr ON hv.id = hr.vacancy_id
+             INNER JOIN hh.employer AS he ON hv.employer_id = he.id
+    GROUP BY hv.id, hv.area_id
+)
+SELECT
+    cq.area_id,
+    ha.city,
+    min(cq.difference_time),
+    max(cq.difference_time)
+FROM calculate_query AS cq
+         LEFT JOIN hh.area AS ha ON cq.area_id = ha.id
+GROUP BY cq.area_id, ha.city;
